b0733318ead5b23f34bdf67bd0970486f0416e91
Akira-Wu <akira.wu@viwave.com>
2019-10-07 15:33:38 +0800

    first commit

---
+++ app/src/main/java/com/mhw/ravenheller/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mhw.ravenheller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
